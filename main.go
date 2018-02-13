261f2279714173e764dc97fba19761f3eeb06301
wim van laer <van.laer.wim@gmail.com>
2018-02-13 23:01:54 +0100

    added track controller in main.go

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"fmt"
-	"github.com/wim07101993/MusicRestService/models"
-	"encoding/json"
+	"github.com/wim07101993/MusicRestService/controllers"
 )
 
 func main() {
@@ -18,57 +17,13 @@ func main() {
 		fmt.Fprint(w, "Hello Brixel!")
 	})
 
+	// Get an instance of the TrackController type
+	tc := controllers.NewTrackController()
+
 	// Add a route to serve a track
-	r.GET("/tracks/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		// Create an example track to serve
-		t := models.Track{
-			Title:"Song 2",
-			Artist: "Blur",
-			Album: "Blur",
-			Genre: "Rock",
-			Id: "1",
-		}
-
-		// Marshal the value to json
-		tj, err := json.Marshal(t)
-		// Check for errors
-		if err!=nil {
-			fmt.Println("Error while trying to marshal track:", err)
-		}
-
-		// Write the content-type, statuscode and content with the response writer
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "%s", tj)
-	})
-
-	r.POST("/tracks", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		// Create empty value to fill with body of request
-		t := models.Track{}
-
-		// Fill the track's fields
-		json.NewDecoder(r.Body).Decode(&t)
-
-		// Add an id
-		t.Id = "someId"
-
-		// Marshal the value to json
-		tj, err := json.Marshal(t)
-		// Check for errors
-		if err!=nil {
-			fmt.Println("Error while trying to marshal track:", err)
-		}
-
-		// Write the content-type, statuscode and content with the response writer
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprintf(w, "%s", tj)
-	})
-
-	r.DELETE("/tracks", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		// TODO: there is no database to remove from yet
-		w.WriteHeader(http.StatusNotImplemented)
-	})
+	r.GET("/tracks/:id", tc.GetTrack)
+	r.POST("/tracks", tc.CreateTrack)
+	r.DELETE("/tracks", tc.DeleteTrack)
 
 	// Give some feedback to the console
 	fmt.Println("Serving on localhost:3000")
