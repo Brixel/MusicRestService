8c80add02d82867efc5533f700e6eb94651d66e2
wim van laer <van.laer.wim@gmail.com>
2018-02-13 22:36:44 +0100

    changed json names of fields of model

--- models/music.go
+++ models/music.go
@@ -3,13 +3,13 @@ package models
 // Track represents the structure of our music data
 type Track struct {
 	// Id is the unique ide by which we can search our music
-	Id     string
+	Id     string	`json:"id"`
 	// Title is the title of the track
-	Title  string
+	Title  string	`json:"title"`
 	// Artist is the artist that created the track
-	Artist string
+	Artist string	`json:"artist"`
 	// Album is the album in which the track is found
-	Album  string
+	Album  string	`json:"album"`
 	// Genre is the genre of the track
-	Genre  string
+	Genre  string	`json:"genre"`
 }
