f7425817803f39be335bdbb6a31449de6ac70db9
wim van laer <van.laer.wim@gmail.com>
2018-02-13 21:37:02 +0100

    Serve hello world

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # MusicRestService
-A rest service that serves music files from a database. This app is built and explained in the talk on the Workshopday2018 of Brixel
+A rest service that serves music files from a database. This app is built and explained in the talk on the 
+Workshopday2018 of Brixel
 
 We start with a simple "Hello world" example to test our develpment environment.
 
 ```Go
 fmt.Println("Hello Brixel!")
 ```
+
+Next we will build a simple http server that serves the hello world message when a user enters a URL. Therefore we 
+need a package that can be downloaded with the <b>"go get"</b> command.
+
+```bash
+$ go get github.com/julienschmidt/httprouter
+```
+
+This will download the httprouter of Julien Schmidt from github to your GOPATH.
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, Brixel!")
+	// Create a value of a router
+	r := httprouter.New()
+
+	// Add a route for the router
+	r.GET("/hello", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Send the message as a response through the response writer
+		fmt.Fprint(w, "Hello Brixel!")
+	})
+
+	// Give some feedback to the console
+	fmt.Println("Serving on localhost:3000")
+
+	// Start the server by listening on the right port
+	err := http.ListenAndServe("localhost:3000", r)
+	// If something goes wrong while serving, notify over the console
+	if err != nil {
+		fmt.Println("Error while trying to serve:", err)
+	}
 }
